5cea2fcbb410b623e4c59e93776d9a5e73faa00a
texatron <teratronsirimus@gmail.com>
2020-07-16 13:15:04 +0300

    LowerRange / UpperRange

--- nn/range.go
+++ nn/range.go
@@ -28,9 +28,7 @@ func (l lowerRangeType) Set(args ...Setter) {
 		Log("Empty Set()", true) // !!!
 	} else {
 		if a, ok := args[0].(NeuralNetwork); ok {
-			if c, ok := l.check().(lowerRangeType); ok {
-				a.Get().Set(c)
-			}
+			a.Get().Set(l)
 		}
 	}
 }
@@ -40,9 +38,7 @@ func (u upperRangeType) Set(args ...Setter) {
 		Log("Empty Set()", true) // !!!
 	} else {
 		if a, ok := args[0].(NeuralNetwork); ok {
-			if c, ok := u.check().(upperRangeType); ok {
-				a.Get().Set(c)
-			}
+			a.Get().Set(u)
 		}
 	}
 }
@@ -68,23 +64,4 @@ func (u upperRangeType) Get(args ...Getter) GetterSetter {
 		}
 	}
 	return nil
-}
-
-// Checker
-func (l lowerRangeType) check() GetterSetter {
-	switch {
-	case l < 0 || l > 1:
-		return DefaultRate
-	default:
-		return l
-	}
-}
-
-func (u upperRangeType) check() GetterSetter {
-	switch {
-	case u < 0 || u > 1:
-		return DefaultRate
-	default:
-		return u
-	}
 }
