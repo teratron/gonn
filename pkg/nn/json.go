1711def2303241e6f4a8b801aa11616fa0066067
teratron <teratronsirimus@gmail.com>
2020-08-14 13:35:36 +0300

    json

--- pkg/nn/json.go
+++ pkg/nn/json.go
@@ -44,37 +44,58 @@ func (n *NN) readJSON(filename string) {
 
 	// Декодируем json в NN
 	err = json.Unmarshal(b, &n)
-	if err != nil { log.Println(err) }
+	if err != nil {
+		log.Println(err)
+	}
 	//fmt.Println(n)
 
 	// Декодируем json в map[string]interface{}
 	var data interface{}
 	err = json.Unmarshal(b, &data)
-	if err != nil { log.Println(err) }
+	if err != nil {
+		log.Println(err)
+	}
 	//fmt.Println(data)
 
 	for key, value := range data.(map[string]interface{}) {
 		if v, ok := value.(map[string]interface{}); ok {
 			if key == "architecture" {
 				b, err = json.Marshal(&v)
-				if err != nil { log.Println(err) }
-				fmt.Println(string(b))
+				if err != nil {
+					log.Println(err)
+				}
+				//fmt.Println(string(b))
 
 				err = json.Unmarshal(b, &data)
-				if err != nil { log.Println(err) }
+				if err != nil {
+					log.Println(err)
+				}
 				//fmt.Println(data)
 
 				for k, v := range data.(map[string]interface{}) {
 					switch k {
 					case "perceptron":
+						//fmt.Printf("%T - %v\n", n.Architecture, n.Architecture)
+
+						if _, ok := n.Architecture.(*perceptron); !ok {
+							n.Architecture = nil
+						}
+
+						n.Architecture = &perceptron{}
+
+						fmt.Printf("%T - %v\n", v, v)
 						b, err = json.Marshal(&v)
-						if err != nil { log.Println(err) }
-						fmt.Println(string(b))
-
-						err = json.Unmarshal(b, &n.Architecture.(*perceptron).Configuration)
-						if err != nil { log.Println(err) }
-						fmt.Println(&n.Architecture.(*perceptron).Configuration)
-
+						if err != nil {
+							log.Println(err)
+						}
+						//fmt.Println(string(b))
+						if a, ok := n.Architecture.(*perceptron); ok {
+							err = json.Unmarshal(b, &a.Configuration)
+							if err != nil {
+								log.Println(err)
+							}
+							fmt.Println(a.Configuration)
+						}
 					case "hopfield":
 						fmt.Printf("%s - %T - %v\n", k, v, v)
 					default:
@@ -83,36 +104,6 @@ func (n *NN) readJSON(filename string) {
 			}
 		}
 	}
-	/*dec := json.NewDecoder(bufio.NewReader(st1))
-	fmt.Println("+-+---",st1)
-	if _, ok := value.(map[string]interface{}); ok {
-		err = dec.Decode(rr)
-		err = json.Unmarshal(strings.(st1), &v)
-		fmt.Printf("---- %T - %v\n", rr, rr)
-	} else {
-		err = dec.Decode(rr.IsTrain)
-		fmt.Printf("---- %T - %v\n", rr.IsTrain, rr.IsTrain)
-	}*/
-	//fmt.Println(n.IsTrain)
-
-	/*aa := data["architecture"].(map[string]interface{})
-	//fmt.Println(aa)
-	dd := aa["perceptron"]
-	//fmt.Println(dd)
-
-	st := os.Stdout
-	enc := json.NewEncoder(st)
-	enc.SetIndent("", "\t")
-	err = enc.Encode(&dd)
-
-	pp := &perceptron{}
-	ppp := pp.Configuration
-	dec := json.NewDecoder(bufio.NewReader(st))
-	if err := dec.Decode(ppp); err == io.EOF {
-		return
-	}*/
-	//fmt.Printf("%T - %v\n", ppp.Bias, ppp.Bias)
-
 }
 
 func (n *NN) writeJSON(filename string) {
