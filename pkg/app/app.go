fc0eab995746f1dfecdf48bb820c3247d5b07683
teratron <teratronsirimus@gmail.com>
2020-10-14 16:28:55 +0300

    dev

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -5,11 +5,13 @@ import "github.com/zigenzoog/gonn/pkg"
 // Declare conformity with Application interface
 var _ Application = (*app)(nil)
 
+// Application
 type Application interface {
 	//
 	pkg.Controller
 }
 
+// app
 type app struct {
 }
 
