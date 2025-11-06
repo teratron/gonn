e289a82e1b9ee69c3e6d61528f672e1c78b8e284
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2025-11-06 14:20:59 +0200

    refactor(pkg/network): optimize Bundle struct and methods - Rename Bundle fields to follow Go conventions (e.g. _number to number) - Update Bundle constructor functions to use new field names - Modify Add method to increment correct fields - Refactor GetMisses method for better performance and readability - Update SetInputs and SetTargets methods to work with Bundle struct - Reorganize FORWARD PROPAGATION METHODS section - Simplify calculateMisses method in Network struct

--- pkg/network/bundle.go
+++ pkg/network/bundle.go
@@ -7,32 +7,32 @@ import (
 )
 
 type Bundle[T utils.Float, S neuron.Nucleus[T]] struct {
-	Cells        []S
-	_number      uint
-	_numberFloat T
+	Cells       []S
+	number      uint
+	numberFloat T
 }
 
 func NewBundle[T utils.Float, S neuron.Nucleus[T]]() Bundle[T, S] {
 	return Bundle[T, S]{
-		Cells:        make([]S, 0),
-		_number:      0,
-		_numberFloat: T(0),
+		Cells:       make([]S, 0),
+		number:      0,
+		numberFloat: T(0),
 	}
 }
 
 func NewBundleWithData[T utils.Float, S neuron.Nucleus[T]](data []T) Bundle[T, S] {
 	number := uint(len(data))
 	return Bundle[T, S]{
-		Cells:        make([]S, 0),
-		_number:      number,
-		_numberFloat: T(float64(number)),
+		Cells:       make([]S, 0),
+		number:      number,
+		numberFloat: T(float64(number)),
 	}
 }
 
 func (b *Bundle[T, S]) Add(cell S) {
 	b.Cells = append(b.Cells, cell)
-	b._number++
-	b._numberFloat = T(float64(b._number))
+	b.number++
+	b.numberFloat = T(float64(b.number))
 }
 
 func (b *Bundle[T, S]) GetValues() []*T {
@@ -43,57 +43,36 @@ func (b *Bundle[T, S]) GetValues() []*T {
 	return values
 }
 
-// Bundle for Output or Hidden.
-
-func (b *Bundle[T, S]) GetMisses() []*T {
-	misses := make([]*T, len(b.Cells))
-	//for i, c := range b.Cells {
-	//	if n, ok := any(c).(neuron.Neuron[T]); ok {
-	//		misses[i] = n.GetMiss()
-	//	} else {
-	//		// For non-Neuron types, return nil or zero value
-	//		zero := T(0)
-	//		misses[i] = &zero
-	//	}
-	//}
-	if c, ok := any(b.Cells).([]neuron.Neuron[T]); ok {
-		for i, n := range c {
-			misses[i] = n.GetMiss()
-		}
-
-	}
-	return misses
-}
-
 // Input Bundle specific methods
 
-//func NewInputBundle[T utils.Float](data []T) InputBundle[T] {
-//	number := uint(len(data))
-//	bundle := InputBundle[T]{
-//		Bundle: Bundle[T, *cell.Input[T]]{
-//			Cells:        make([]*cell.Input[T], 0),
-//			_number:      number,
-//			_numberFloat: T(float64(number)),
-//		},
-//	}
-//
-//	if len(data) > 0 {
-//		for _, v := range data {
-//			value := v
-//			bundle.Cells = append(bundle.Cells, cell.NewInput(&value))
+//	func NewInputBundle[T utils.Float](data []T) InputBundle[T] {
+//		number := uint(len(data))
+//		bundle := InputBundle[T]{
+//			Bundle: Bundle[T, *cell.Input[T]]{
+//				Cells:        make([]*cell.Input[T], 0),
+//				number:      number,
+//				numberFloat: T(float64(number)),
+//			},
 //		}
-//	}
 //
-//	return bundle
-//}
-//
-//func (b *InputBundle[T]) SetInputs(data []T) {
-//	for i, v := range data {
-//		if i < len(b.Cells) {
-//			b.Cells[i].SetValue(&v)
+//		if len(data) > 0 {
+//			for _, v := range data {
+//				value := v
+//				bundle.Cells = append(bundle.Cells, cell.NewInput(&value))
+//			}
 //		}
+//
+//		return bundle
 //	}
-//}
+func (b *Bundle[T, _]) SetInputs(data *[]T) {
+	if i, ok := any(b).(*cell.Input[T]); ok {
+		for j, v := range *data {
+			if j < len(b.Cells) {
+				i.SetValue(&v)
+			}
+		}
+	}
+}
 
 // Output Bundle specific methods
 
@@ -102,8 +81,8 @@ func (b *Bundle[T, S]) GetMisses() []*T {
 //	bundle := OutputBundle[T]{
 //		Bundle: Bundle[T, *cell.Output[T]]{
 //			Cells:        make([]*cell.Output[T], 0),
-//			_number:      number,
-//			_numberFloat: T(float64(number)),
+//			number:      number,
+//			numberFloat: T(float64(number)),
 //		},
 //	}
 //
@@ -117,7 +96,7 @@ func (b *Bundle[T, S]) GetMisses() []*T {
 //	return bundle
 //}
 
-func (b *Bundle[T, S]) SetTargets(data *[]T) {
+func (b *Bundle[T, _]) SetTargets(data *[]T) {
 	if o, ok := any(b).(*cell.Output[T]); ok {
 		for i, v := range *data {
 			if i < len(b.Cells) {
@@ -134,8 +113,8 @@ func (b *Bundle[T, S]) SetTargets(data *[]T) {
 //	bundle := HiddenBundle[T]{
 //		Bundle: Bundle[T, *cell.Hidden[T]]{
 //			Cells:        make([]*cell.Hidden[T], 0),
-//			_number:      number,
-//			_numberFloat: T(float64(number)),
+//			number:      number,
+//			numberFloat: T(float64(number)),
 //		},
 //	}
 //
@@ -151,6 +130,19 @@ func (b *Bundle[T, S]) SetTargets(data *[]T) {
 // ----------------------------------------------------------------------------
 // FORWARD PROPAGATION METHODS
 // ----------------------------------------------------------------------------
+
+// Bundle for Output or Hidden
+
+func (b *Bundle[T, _]) GetMisses() []*T {
+	misses := make([]*T, b.number)
+	if c, ok := any(b.Cells).([]neuron.Neuron[T]); ok {
+		for i, n := range c {
+			misses[i] = n.GetMiss()
+		}
+
+	}
+	return misses
+}
 
 func (b *Bundle[T, _]) calculateValues() {
 	if c, ok := any(b.Cells).([]neuron.Neuron[T]); ok {
--- pkg/network/propagation.go
+++ pkg/network/propagation.go
@@ -2,24 +2,17 @@ package network
 
 import (
 	"github.com/teratron/gonn/pkg/loss"
-	"github.com/teratron/gonn/pkg/neuron/cell"
-	"github.com/teratron/gonn/pkg/utils"
 )
 
 // ----------------------------------------------------------------------------
 // FORWARD PROPAGATION METHODS
 // ----------------------------------------------------------------------------
 
-// CalculateValues calculates the value of all neurons in the network
 func (n *Network[T]) calculateValues() {
-	//for _, c := range n.Cells {
-	//	c.CalculateValue()
-	//}
 	n.Hidden.calculateValues()
 	n.Output.calculateValues()
 }
 
-// CalculateLoss calculates and returns the total error of the output neurons
 func (n *Network[T]) calculateLoss(mode loss.Type) T {
 	return loss.CalculateTotalLoss(n.Output.GetMisses(), mode)
 }
@@ -28,38 +21,17 @@ func (n *Network[T]) calculateLoss(mode loss.Type) T {
 // BACKWARD PROPAGATION METHODS
 // ----------------------------------------------------------------------------
 
-// CalculateMisses calculates the error of hidden neurons
-// Implements backward propagation by processing neurons in reverse order
 func (n *Network[T]) calculateMisses() *Network[T] {
-	// Process hidden neurons in reverse order for backpropagation
-	cells := n.Hidden.Cells
-	for i := len(cells) - 1; i >= 0; i-- {
-		calculateMissForHidden(cells[i])
+	for i := n.Hidden.number - 1; i >= 0; i-- {
+		var cum T = 0.0
+		for _, a := range n.Hidden.Cells[i].OutgoingAxons {
+			cum += a.CalculateMiss()
+		}
 	}
 	return n
 }
 
-// Helper function to calculate miss for a hidden neuron
-// This simulates the calculate_miss() method from the Rust implementation
-func calculateMissForHidden[T utils.Float](neuron *cell.Hidden[T]) {
-	// Accumulate error from each outgoing connection using the axon's CalculateMiss method logic
-	// In backpropagation, the error contribution from each outgoing axon is:
-	// error_from_next_layer * weight_of_connection
-	var cum T = 0.0
-	for _, a := range neuron.OutgoingAxons {
-		// Calculate the error contribution from this axon to the current neuron
-		cum += a.CalculateMiss()
-
-	}
-	neuron.SetMiss(cum)
-}
-
-// CalculateWeights updates weights of all neurons in the network
 func (n *Network[T]) calculateWeights(rate *T) {
-	// Update weights for all neurons in the network
-	//for _, c := range n.Cells {
-	//	c.CalculateWeight(rate)
-	//}
 	n.Hidden.calculateWeights(rate)
 	n.Output.calculateWeights(rate)
 }
