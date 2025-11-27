3e7b6895d3824d3981e64e02b962d89c47291fe4
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2025-11-27 12:39:46 +0200

    refactor(layer): simplify generic type parameters in input and output layers
    Update Input and Output layer types to remove unnecessary generic
    parameters and use concrete types for better readability and
    maintainability. The changes affect the type signatures of both
    layers to use *cell.Input[T] and *cell.Output[T] directly instead
    of generic parameters.

--- pkg/layer/input.go
+++ pkg/layer/input.go
@@ -6,19 +6,19 @@ import (
 	"github.com/teratron/gonn/pkg/utils"
 )
 
-type Input[T utils.Float, S neuron.Nucleus[T]] core[T, S]
+type Input[T utils.Float] core[T, *cell.Input[T]]
 
-func NewInput[T utils.Float](size int) *Input[T, *cell.Input[T]] {
-	i := &Input[T, *cell.Input[T]]{}
+func NewInput[T utils.Float](size int) *Input[T] {
+	i := &Input[T]{}
 	i.Init(0)
 	utils.Logger.Info("Input layer created", "size", 0)
 	return i
 }
 
-func (i *Input[T, S]) Init(size int) {
+func (i *Input[T]) Init(size int) {
 	i.Type = neuron.INPUT
 	i.Id = 0
 	i.Size = uint(size)
-	i.cells = make([]S, size)
+	i.cells = make([]*cell.Input[T], size)
 	utils.Logger.Info("Input layer created", "size", size)
 }
--- pkg/layer/output.go
+++ pkg/layer/output.go
@@ -8,19 +8,19 @@ import (
 	"github.com/teratron/gonn/pkg/utils"
 )
 
-type Output[T utils.Float, S neuron.Neuron[T]] struct {
-	*Dense[T, S]
+type Output[T utils.Float] struct {
+	*Dense[T, *cell.Output[T]]
 	Loss loss.Type `json:"loss" xml:"loss"`
 }
 
-func NewOutput[T utils.Float](size int, activation activation.Type, loss loss.Type, bias bool) *Output[T, *cell.Output[T]] {
-	o := &Output[T, *cell.Output[T]]{}
+func NewOutput[T utils.Float](size int, activation activation.Type, loss loss.Type, bias bool) *Output[T] {
+	o := &Output[T]{}
 	o.Init(size, activation, loss, bias)
 	utils.Logger.Info("Output layer created", "size", size, "activation", activation.String(), "loss", loss.String(), "bias", bias)
 	return o
 }
 
-func (o *Output[T, S]) Init(size int, activation activation.Type, loss loss.Type, bias bool) {
+func (o *Output[T]) Init(size int, activation activation.Type, loss loss.Type, bias bool) {
 	o.Type = neuron.OUTPUT
 	o.Id = 0
 	o.Dense.Init(size, activation, bias)
